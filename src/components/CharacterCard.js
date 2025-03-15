fc15a6ae48e6f61e3dbfd2b243f3fbbe30ce1a12
monicaortiz20 <ortiznavajomonica@gmail.com>
2025-03-15 18:14:14 +0100

    handle clicks events - creating DetailPAge

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { FaHeart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function CharacterCard({ character, addFavorite, deleteFavorite, isFavorite }) {
+  const navigate = useNavigate();
+
   //verificamos si existe como favorito o no
   function handleFavorites() {
     if (isFavorite) {
@@ -11,9 +14,13 @@ function CharacterCard({ character, addFavorite, deleteFavorite, isFavorite }) {
     }
   }
 
+  function seeCharacterDetail() {
+    navigate(`/detail/${character.id}`);
+  }
+
   return (
     <div className="col">
-      <div className="card h-100 shadow">
+      <div className="card h-100 shadow" onClick={seeCharacterDetail}>
         <img
           src={character.image}
           className="card-img-top"
@@ -21,7 +28,13 @@ function CharacterCard({ character, addFavorite, deleteFavorite, isFavorite }) {
         />
         <div className="card-body">
           <h5 className="card-title">{character.name}</h5>
-          <button className="btn btn-primary" onClick={handleFavorites}>
+          <button
+            className="btn btn-primary"
+            onClick={(e) => {
+              e.stopPropagation(); //para que el evento click de 'favorite' no aplique al padre
+              handleFavorites();
+            }}
+          >
             <FaHeart
               className={`heart-icon ${isFavorite ? "text-danger" : ""}`}
             />
